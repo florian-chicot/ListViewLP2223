84d5ad2323b60bdab303dd321dc54a904b895c1b
Florian <105889597+florian-chicot@users.noreply.github.com>
2022-12-06 16:27:30 +0100

    Add Task model

---
+++ app/src/main/java/Model/Task.java
@@ -0,0 +1,54 @@
+package Model;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private boolean isFinished;
+
+    public Task(int id, String title, String description, boolean isFinished) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.isFinished = isFinished;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(boolean finished) {
+        isFinished = finished;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+}
